aa0028097d8886b9818a0ab7e1cfdc598169ac4a
Julio Campos Machado <32886080+juliocamposmachado@users.noreply.github.com>
2025-10-22 13:36:47 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -205,10 +205,7 @@ function App() {
             Sistema de leitura de runas Elder Futhark com sorteio criptograficamente seguro
           </p>
           <p className="text-xs opacity-75">
-            As runas não predizem o futuro, mas oferecem perspectivas para reflexão 
-          </p>
-           <p className="text-xs opacity-75">
-            Feito por Julio Campos Machado com ❤
+            As runas não predizem o futuro, mas oferecem perspectivas para reflexão
           </p>
         </footer>
       </div>
